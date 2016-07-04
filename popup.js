0c3760921e1daf947e8b136c2c60b199c48da476
Mark A. Ropper <mark@chavfreezone.me.uk>
2016-07-04 12:14:28 +0100

    Updated http to https

--- popup.js
+++ popup.js
@@ -4,8 +4,8 @@ $(emergency).click(
         chrome.storage.sync.get({
             religiousEnabled: false
         }, function(items) {
-            $.get("http://emergency.nofap.com/director.php", {cat:"em", religious:items.religiousEnabled ? "true" : "false", platform:"extension"}, function(e){chrome.tabs.update({url: e})});
-        });        
+            $.get("https://emergency.nofap.com/director.php", {cat:"em", religious:items.religiousEnabled ? "true" : "false", platform:"extension"}, function(e){chrome.tabs.update({url: e})});
+        });
     }
 );
 
@@ -15,7 +15,7 @@ $(depression).click(
         chrome.storage.sync.get({
             religiousEnabled: false
         },function(items) {
-            $.get("http://emergency.nofap.com/director.php", {cat:"dep", religious:items.religiousEnabled ? "true" : "false", platform:"extension"}, function(e){chrome.tabs.update({url: e})});
+            $.get("https://emergency.nofap.com/director.php", {cat:"dep", religious:items.religiousEnabled ? "true" : "false", platform:"extension"}, function(e){chrome.tabs.update({url: e})});
         });
     }
 );
@@ -26,7 +26,7 @@ $(rejection).click(
         chrome.storage.sync.get({
             religiousEnabled: false
         },function(items) {
-            $.get("http://emergency.nofap.com/director.php", {cat:"rej", religious:items.religiousEnabled ? "true" : "false", platform:"extension"}, function(e){chrome.tabs.update({url: e})});
+            $.get("https://emergency.nofap.com/director.php", {cat:"rej", religious:items.religiousEnabled ? "true" : "false", platform:"extension"}, function(e){chrome.tabs.update({url: e})});
         });
     }
 );
@@ -37,7 +37,7 @@ $(relapsed).click(
         chrome.storage.sync.get({
             religiousEnabled: false
         },function(items) {
-            $.get("http://emergency.nofap.com/director.php", {cat:"rel", religious:items.religiousEnabled ? "true" : "false", platform:"extension"}, function(e){chrome.tabs.update({url: e})});
-        });        
+            $.get("https://emergency.nofap.com/director.php", {cat:"rel", religious:items.religiousEnabled ? "true" : "false", platform:"extension"}, function(e){chrome.tabs.update({url: e})});
+        });
     }
 );
